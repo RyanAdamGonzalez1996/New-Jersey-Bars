e601c6a0808dbd7f7760e78d810e94a8e724a235
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-01-26 20:34:53 -0500

    Added the framework for the top ten bars graph.

---
+++ js/topTenVisualization.js
@@ -0,0 +1,26 @@
+// Create Top Ten Bars Chart (Bar Chart)
+
+// SVG Dimensions
+
+// Plot Dimensions
+
+// Create SVG Wrapper
+
+// Append SVG group that contains our plot
+
+// CSV file path (bar_descriptions)
+
+// Read in CSV data
+
+    // Declare the Data as numerals
+
+    // Create Scale for X Coordinates
+
+    // Create Scale for Y Coordinates
+
+    // Create Axis Functions
+
+    // Append Axis to Plot
+
+    // Draw the bars on the plot
+    
