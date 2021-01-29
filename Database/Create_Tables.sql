0a6512282f665caed4abd8bf012a599fe5d4addc
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-01-28 20:36:08 -0500

    Updated again

--- Database/Create_Tables.sql
+++ Database/Create_Tables.sql
@@ -1,11 +1,10 @@
-DROP TABLE bar_description
+DROP TABLE bar_description;
 
 CREATE TABLE bar_description (
     bar_id serial   NOT NULL,
-    bar_name varchar   NOT NULL,
+    restaurant_name varchar   NOT NULL,
     bar_type varchar   NOT NULL,
-    avg_price varchar   NOT NULL,
-    ratings decimal   NOT NULL,
-    address varchar NOT NULL
+    price_name varchar   NOT NULL,
+    rating decimal   NOT NULL,
+    address_list varchar NOT NULL
 );
-
