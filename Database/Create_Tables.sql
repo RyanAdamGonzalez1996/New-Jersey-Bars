1582b4a86323b11578e70515223a885e1221a4be
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-01-28 20:10:43 -0500

    Updated Database.

--- Database/Create_Tables.sql
+++ Database/Create_Tables.sql
@@ -1,26 +1,11 @@
-DROP TABLE bar_description, bar_location
+DROP TABLE bar_description
 
 CREATE TABLE bar_description (
     bar_id serial   NOT NULL,
     bar_name varchar   NOT NULL,
     bar_type varchar   NOT NULL,
-    website varchar   NOT NULL,
-    hours_of_operation varchar   NOT NULL,
-    phone_number int   NOT NULL,
     avg_price varchar   NOT NULL,
     ratings decimal   NOT NULL,
-    PRIMARY KEY (bar_id)
-);
-
-CREATE TABLE bar_location (
-   	bar_id int   NOT NULL,
-    coordinates varchar   NOT NULL,
-    county varchar   NOT NULL,
-    streetAddress varchar   NOT NULL,
-    town varchar   NOT NULL,
-    state varchar   NOT NULL,
-    zipcode varchar   NOT NULL,
-	FOREIGN KEY (bar_id)
-	REFERENCES bar_description (bar_id)
+    address varchar NOT NULL
 );
 
