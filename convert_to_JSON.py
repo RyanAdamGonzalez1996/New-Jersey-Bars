16894b2651b5c2c418136699e47060c57fd72a5b
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-01-30 10:05:05 -0500

    Final

--- convert_to_JSON.py
+++ convert_to_JSON.py
@@ -1,13 +1,6 @@
 import csv, json
 
 # Read in the CSV files from our database tables
-atlanticCity = "csv/Alantic_City_data.csv"
-flemington = "csv/Flemington_data.csv"
-morristown = "csv/Morristown_data.csv"
-newBrunswick = "csv/New_Brunswick_data.csv"
-princeton = "csv/Princeton_data.csv"
-somerville = "csv/Somerville_data.csv"
-trenton = "csv/Trenton_data.csv"
 allCity = "csv/allCity_Geocode.csv"
 
 # All City
