f9e47518b9e29709f62024b306864e1f0e0d6fc2
RyanAdamGonzalez1996 <70239700+RyanAdamGonzalez1996@users.noreply.github.com>
2021-02-01 13:23:51 -0500

    Working flask app
    Not entirely set up as the database is giving issues with the use of the allCity_GeoCode.csv file

--- app.py
+++ app.py
@@ -14,19 +14,26 @@ jsonDict = {}
 # Create Routes
 @app.route("/")
 def init():
-    # Create a connection to posgresql database
-    conn = psycopg2.connect("dbname= Project2_Group5 user=Group5 password=group5!")
+    hostname = "localhost"
+    dbname = "Project2_Group5"
+    user = "Group5"
+    password = "group5!"
+
+    conn = psycopg2.connect(host=hostname, dbname=dbname, user=user, password=password)
 
     cursor = conn.cursor()
+
+    #with open('csv/alantic_City_data.csv', 'r') as f:
+    #    next(f) # Skip the header row.
+    #    cursor.copy_from(f,'bar_description')
+
+    conn.commit()
 
     cursor.execute("SELECT * FROM bar_description")
 
     results = cursor.fetchall()
-    
-    jsonDict = results
-    conn.close()
 
-    return jsonDict
+    return json.dumps(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
