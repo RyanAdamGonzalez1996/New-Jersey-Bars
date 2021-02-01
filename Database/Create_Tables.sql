1d2fc4fa1da68d8081a5276845ea4daa6c34159e
RyanAdamGonzalez1996 <70239700+RyanAdamGonzalez1996@users.noreply.github.com>
2021-02-01 13:28:09 -0500

    Update Create_Tables.sql

--- Database/Create_Tables.sql
+++ Database/Create_Tables.sql
@@ -1,10 +1,9 @@
 DROP TABLE bar_description;
 
 CREATE TABLE bar_description (
-    bar_id serial   NOT NULL,
     restaurant_name varchar   NOT NULL,
     bar_type varchar   NOT NULL,
-    price_name varchar   NOT NULL,
-    rating decimal   NOT NULL,
-    address_list varchar NOT NULL
+    price_level varchar   NOT NULL,
+    rating_list varchar   NOT NULL,
+    address_list varchar NOT NULL,
 );
